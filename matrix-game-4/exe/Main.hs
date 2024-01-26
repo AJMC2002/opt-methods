57dc547420bed5f19b2d7453e42edaf304722e91
AJMC2002 <adrianmosquera2015@gmail.com>
2024-01-26 11:11:24 +1000

    Lab 4

--- matrix-game-4/exe/Main.hs
+++ matrix-game-4/exe/Main.hs
@@ -6,18 +6,27 @@ import MatrixGame (highStrategy, lowStrategy)
 import Optimization.DirectTask (solveDirect)
 import Optimization.DualTask (solveDual)
 import Prelude as P
+import System.Random qualified as R
 
 main :: IO ()
 main = do
-    let a = resize' (Sz2 6 8) $ fromList (ParN 0) [11, 13, 10, 14, 9, 19, 3, 4, 4, 5, 17, 16, 11, 12, 9, 15, 5, 10, 11, 5, 19, 4, 11, 16, 1, 4, 7, 2, 5, 0, 16, 5, 19, 12, 13, 17, 3, 4, 18, 13, 3, 7, 11, 5, 8, 4, 2, 10]
+    let 
+        -- Initial values
+        salt = 19092002
+        gen = R.mkStdGen salt
+        rng = (-50 :: Double, 50)
+        comp = ParN 0
+        a = computeP $ uniformRangeArray gen rng comp (Sz2 6 8) :: Matrix P Double
         b = A.replicate (ParN 0) 6 1
         c = A.replicate (ParN 0) 8 1
         beta = minimum' a
-        a' = a .+ beta
+        a' = a .+ abs beta
 
     putStrLn $ showMat a 2
-    putStrLn $ showVec b 2
-    putStrLn $ showVec c 2
+    putStrLn $ "underline(A) = " ++ show (lowStrategy a)
+    putStrLn $ "overline(A) = " ++ show (highStrategy a)
+    putStrLn $ "beta = " ++ show beta
+    putStrLn $ showMat a' 2
 
     (y, alfa1) <- solveDirect a' b c
     putStrLn $ "y = " ++ showVec y 2
@@ -31,4 +40,4 @@ main = do
 
     putStrLn $ "underline(A) = " ++ show (lowStrategy a)
     putStrLn $ "overline(A) = " ++ show (highStrategy a)
-    putStrLn $ "beta = " ++ show (1 / alfa1 - abs beta)
+    putStrLn $ "phi = " ++ show (1 / alfa1 - abs beta)
